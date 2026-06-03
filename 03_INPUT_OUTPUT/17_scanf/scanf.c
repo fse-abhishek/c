c804537b5dfdf4fef68b7b7846f9ed53500eab77
Abhishek Nishad <abnsa597@gmail.com>
2026-06-03 13:13:50 +0530

    new code

--- 03_INPUT_OUTPUT/17_scanf/scanf.c
+++ 03_INPUT_OUTPUT/17_scanf/scanf.c
@@ -1,8 +1,25 @@
-#include<stdio.h>
-int main (){
-    int a= 4,b= 6;
-    printf("value a is = %d and value b is = %d",a , b);
-    return 0;
+#include <stdio.h>
 
-    
-}
+int main() {
+    char name[50];
+    int age;
+    float salary;
+
+    printf("===== Employee Information System =====\n");
+
+    printf("Enter your name: ");
+    scanf("%49s", name);
+
+    printf("Enter your age: ");
+    scanf("%d", &age);
+
+    printf("Enter your salary: ");
+    scanf("%f", &salary);
+
+    printf("\n===== Employee Details =====\n");
+    printf("Name   : %s\n", name);
+    printf("Age    : %d years\n", age);
+    printf("Salary : %.2f\n", salary);
+
+    return 0;
+}   
