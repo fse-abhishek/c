cbd6973e71ff5eb9a3b48a60d0948341edc45ed7
Abhishek Nishad <abnsa597@gmail.com>
2026-06-05 18:49:02 +0530

    logical operator

---
+++ 04_OPERATORS/25_Logical/logic1.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    int y,x=5;
+    y=!x>4;   //  ! operater convert true to false
+    printf("%d",y);
+
+    return 0;
+}
---
+++ 04_OPERATORS/25_Logical/logic2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main ()
+
+{
+
+    int y,x=5;
+
+       y = x > 4 && x < 10;    // condition true
+
+     printf("%d",y );
+
+        return 0;
+
+}
