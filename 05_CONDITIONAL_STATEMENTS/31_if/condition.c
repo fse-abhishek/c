0bfb934b04abcadab5d346476e13e7881db55b1b
Abhishek Nishad <abnsa597@gmail.com>
2026-06-05 19:49:32 +0530

    condition

---
+++ 05_CONDITIONAL_STATEMENTS/31_if/condition.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main ()
+
+{
+    int x;
+    printf("enter the number");
+    scanf("%d",&x);
+    if ("x<18")
+    {
+        printf("this is positive number");
+
+    }
+    return 0;
+}
