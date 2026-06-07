8a92c1e934ac0723858094bc7052c67565225932
Abhishek Nishad <abnsa597@gmail.com>
2026-06-07 14:08:05 +0530

    check for voting eligiblity

---
+++ 05_CONDITIONAL_STATEMENTS/32_if_else/if_else.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main ()
+{
+     int x;
+     printf("enter the number");
+     scanf("%d", &x);
+     if(x>=0)
+     {
+        printf("positive number");
+     }
+
+     else
+        {
+            printf ("negative  number");
+        }
+return 0;
+}
