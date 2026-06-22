49ab7e25ed4876bf9bd1b48a952c6b0425f32dba
Abhishek Nishad <abnsa597@gmail.com>
2026-06-22 17:46:57 +0530

    string use in poiner

---
+++ 11_POINTERS/68_Pointers_And_Strings/pointerstr.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    char *str = "Hello, World!";
+
+    printf("%s\n", str);      // Print the whole string
+    printf("%c\n", *str);     // Print first character
+
+    return 0;
+}
